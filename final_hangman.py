9603b6881a64a1c9e13334fefce4135e50468cab
dry-navy <vd090904@gmail.com>
2025-06-05 10:15:08 +0530

    Update final_hangman.py

--- final_hangman.py
+++ final_hangman.py
@@ -19,39 +19,39 @@ print(blanks)
 
 condition = False
 correct_letter = []
-
-while not condition:
-    guess = input("Guess a letter: ").lower()
-    if guess in correct_letter:
-        print(f"You have already guessed {guess}")
-    display = ""
-
-    for letter in chosen_word:
-        if letter == guess:
-            display += letter
-            correct_letter.append(letter)
-        elif letter in correct_letter:
-            display += letter
-        else:
-            display += "_"
-    print(display)
-
-    if guess not in chosen_word:
-
-        lives -= 1
-        print(f"You guessed {guess}, that's not in word. You lose a lfe.")
-        print(stages[lives])
-        if lives == 0:
-            game_over = True
-            print("Your HANGMAN is DEAD. \n*******GAME OVER*******")
+while True:
+    while not condition:
+        guess = input("Guess a letter: ").lower()
+        if guess in correct_letter:
+            print(f"You have already guessed {guess}")
+        display = ""
+    
+        for letter in chosen_word:
+            if letter == guess:
+                display += letter
+                correct_letter.append(letter)
+            elif letter in correct_letter:
+                display += letter
+            else:
+                display += "_"
+        print(display)
+    
+        if guess not in chosen_word:
+    
+            lives -= 1
+            print(f"You guessed {guess}, that's not in word. You lose a lfe.")
+            print(stages[lives])
+            if lives == 0:
+                game_over = True
+                print("Your HANGMAN is DEAD. \n*******GAME OVER*******")
+                print(f"Your word was {chosen_word}")
+                exit()
+    
+        if "_" not in display:
+            condition = True
+            print("*******YOU WON********")
             print(f"Your word was {chosen_word}")
             exit()
-
-    if "_" not in display:
-        condition = True
-        print("*******YOU WON********")
-        print(f"Your word was {chosen_word}")
-        exit()
 
     # print(stages[lives])
 
